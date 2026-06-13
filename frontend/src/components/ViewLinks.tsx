2d8c283bed386dabb07f77a5f87421a6be94d422
Messiah Ridgley II <ridgleymessiah@gmail.com>
2026-06-13 15:58:05 -0400

    add simple display of past created links

--- frontend/src/components/ViewLinks.tsx
+++ frontend/src/components/ViewLinks.tsx
@@ -1,23 +1,52 @@
+import { useState } from "react";
 import type { Session } from "@supabase/supabase-js";
 
-export default function ViewLinks({ session }: { session: Session | null }) {
-  const token = session?.access_token;
+type LinkResponse = {
+  original_url: string;
+  slug: string;
+  owner_id: string;
+  created_at: string;
+};
 
+export default function ViewLinks({ session }: { session: Session | null }) {
+  const [links, setLinks] = useState<LinkResponse[] | null>(null);
+
+  const token = session?.access_token;
   const apiUrl = import.meta.env.VITE_API_URL;
 
   async function fetchLinks() {
     const response = await fetch(`${apiUrl}/links`, {
+      method: "GET",
       headers: {
         Authorization: `Bearer ${token}`,
       },
     });
-    const data = await response.json();
-    console.log(data);
+
+    if (response.ok) {
+      const data = await response.json();
+      setLinks(data);
+    } else {
+      const error = await response.json();
+      console.log(error);
+    }
   }
 
   return (
-    <div>
+    <>
       <button onClick={fetchLinks}>Fetch Links</button>
-    </div>
+      <ul>
+        {links ? links.map((link) => (
+          <li key={link.slug}>
+            <p>
+              {link.slug} -&gt; {link.original_url}
+            </p>
+          </li>
+        )) :
+          <p>
+            Links not found.
+          </p>
+        }
+      </ul>
+    </>
   );
 }
